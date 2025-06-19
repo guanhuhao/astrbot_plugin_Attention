14c896bfa64ffd7e4a407baaf47dee2f17fe376f
guanhuhao <1057368050@qq.com>
2025-06-19 11:28:23 +0800

    git add init version

--- main.py
+++ main.py
@@ -11,14 +11,35 @@ class MyPlugin(Star):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
     
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.command("Attention")
+    async def Attention(self, event: AstrMessageEvent):
+        """
+        这是一个 Attention 指令
+        使用方法:
+        /Attention on 开启群聊注意力
+        /Attention off 关闭群聊注意力
+        """
+        pass
+
+    @Attention.command("on")
+    async def Attention_on(self, event: AstrMessageEvent):
+        """
+        开启群聊注意力
+        """
+        self.offset = 1
+        pass
+
+    @Attention.command("off")
+    async def Attention_off(self, event: AstrMessageEvent):
+        """
+        关闭群聊注意力
+        """
+        self.offset = 0
+        pass
+
+    @filter.event_message_type(filter.EventMessageType.ALL)
+    async def on_all_message(self, event: AstrMessageEvent):
+        yield event.plain_result("收到了一条消息。")
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
